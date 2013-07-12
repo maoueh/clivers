ed3cffdbabbc078791f5b3662eda7539c9305ff8
Matthieu Vachon <matthieu.o.vachon@gmail.com>
2013-07-12 18:41:48 -0400

    Improved Helper::String to perserve paragraphs when formatting text

--- lib/clivers/helper/string.rb
+++ lib/clivers/helper/string.rb
@@ -17,20 +17,43 @@ module Clivers
       end
 
       def self.tokenize(text)
-        text.scan(/([^\s]+)|(\s+)/).map do |word, space|
-          { :text => word ||space, :type => word ? :word : :space }
+        text.scan(/([^\s]+)|(\n\r|\n|\r)|(\s+)/).map do |word, newline, space|
+          case
+          when word
+            { :text => word, :type => :word }
+          when newline
+            { :text => newline, :type => :newline }
+          when space && space =~ /^\s*$/
+            { :text => space, :type => :space }
+          end
         end
       end
 
       def self.tokens_to_lines(tokens, options = {})
         max_width = options[:max_width] || 80;
         indentation = options[:indentation] || ""
+        preserve_paragraphs = options[:preserve_paragraphs] == nil ? true : options[:preserve_paragraphs]
 
         lines = []
         buffer = indentation
 
+        newline_count = 0
         tokens.each do |token|
-          text = token[:type] == :space ? " " : token[:text]
+          type = token[:type]
+          if type == :newline
+            newline_count += 1
+            next
+          end
+
+          if preserve_paragraphs and newline_count > 1
+            for i in 1..newline_count
+              lines << normalize_line(buffer)
+              buffer = indentation
+            end
+          end
+
+          newline_count = 0;
+          text = type == :space ? " " : token[:text]
           potential_width = text.length() + buffer.length()
           if potential_width > max_width
             lines << normalize_line(buffer)
